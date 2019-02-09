9559089d16f498183fd7ce13012940a58389aea1
Nicholj25 <Nicholj25@gmail.com>
2019-02-09 15:51:31 -0500

    Add files via upload

---
+++ public/character.js
@@ -0,0 +1,89 @@
+
+//Create table at start
+var table = buildTable(null);
+var body = document.body;
+table.id = "mainTable";
+body.appendChild(table);
+
+document.addEventListener("DOMContentLoaded", bindButtons);
+
+function bindButtons(){
+    initTable();
+
+    document.getElementById("submitButton").addEventListener("click", function(event){
+        
+        if (document.getElementById("name").value == "") {
+            console.log("Error, name field required!");
+            return;
+        }
+
+        var req = new XMLHttpRequest();
+
+        var insertURL = createQueryURL("/insert");
+
+        req.open("GET", insertURL , true);
+
+        req.addEventListener("load", function(){
+            var response = JSON.parse(req.responseText);
+            initTable();
+        });
+
+        req.send(null);
+        event.preventDefault();
+    })
+}
+
+function buildTable(queryData) {
+    var table = document.createElement("table");
+    table.appendChild(document.createElement("thead"));
+    table.firstElementChild.appendChild(document.createElement("tr"));
+    table = table.firstElementChild.firstElementChild;
+
+    var dataNames = ["Name", "Species", "Year Released", "Year Added to Smash", "Original Series"];
+    dataNames.forEach( function (element) {
+
+        var newItem = document.createElement("th");
+        newItem.textContent = element;
+        newItem.id = "H" + element;
+        table.appendChild(newItem);
+    });
+    table = table.parentElement.parentElement;
+
+    table.appendChild(document.createElement("tbody"));
+    table = table.children[1];
+
+    table = table.parentElement;
+
+    table.style.borderStyle = "solid";
+
+    //Styling the Table
+    var tableBlocks = table.getElementsByTagName("th");
+    for (var i = 0; i < tableBlocks.length; i++) {
+        tableBlocks[i].style.borderStyle = "solid";
+        tableBlocks[i].style.backgroundColor = "lightgreen";
+    }
+
+    tableBlocks = table.getElementsByTagName("td");
+    for (var i = 0; i < tableBlocks.length; i++) {
+        tableBlocks[i].style.borderStyle = "solid";
+    }
+
+    return table;
+}
+
+function initTable() {
+    var req = new XMLHttpRequest();
+    req.open("GET", "/all", true);
+
+    req.addEventListener("load", function(){
+        var response = JSON.parse(req.responseText);
+        var response = JSON.parse(response.results);
+        var updatedTable = buildTable(response);
+        body.removeChild(document.getElementById("mainTable"));
+        updatedTable.id = "mainTable";
+        body.appendChild(updatedTable);
+    });
+
+    req.send(null);
+    event.preventDefault();
+}
---
+++ public/smash_games.js
@@ -0,0 +1,85 @@
+
+//Create table at start
+var table = buildTable(null);
+var body = document.body;
+table.id = "mainTable";
+body.appendChild(table);
+
+document.addEventListener("DOMContentLoaded", bindButtons);
+
+function bindButtons(){
+    initTable();
+}
+
+function buildTable(queryData) {
+    var table = document.createElement("table");
+    table.appendChild(document.createElement("thead"));
+    table.firstElementChild.appendChild(document.createElement("tr"));
+    table = table.firstElementChild.firstElementChild;
+
+    var dataNames = ["Name", "Year Released"];
+    dataNames.forEach( function (element) {
+
+        var newItem = document.createElement("th");
+        newItem.textContent = element;
+        newItem.id = "H" + element;
+        table.appendChild(newItem);
+    });
+    table = table.parentElement.parentElement;
+	
+	var tbody = document.createElement("tbody");
+	var row1 = document.createElement("tr");
+	var row1 = document.createElement("tr");
+	
+	var cell = document.createElement("td");
+	var cell2 = document.createElement("td");
+	var cell3 = document.createElement("td");
+	var cell4 = document.createElement("td");
+	
+	row1.appendChild(cell);
+	row1.appendChild(cell2);
+	row2.appendChild(cell3);
+	row2.appendChild(cell4);
+	
+	tbody.appendChild(trow1);
+	tbody.appendChild(trow2);
+	
+    table.appendChild(tbody);
+	
+    table = table.children[1];
+
+    table = table.parentElement;
+
+    table.style.borderStyle = "solid";
+
+    //Styling the Table
+    var tableBlocks = table.getElementsByTagName("th");
+    for (var i = 0; i < tableBlocks.length; i++) {
+        tableBlocks[i].style.borderStyle = "solid";
+        tableBlocks[i].style.backgroundColor = "lightgreen";
+    }
+
+    tableBlocks = table.getElementsByTagName("td");
+    for (var i = 0; i < tableBlocks.length; i++) {
+        tableBlocks[i].style.borderStyle = "solid";
+    }
+
+    return table;
+}
+
+function initTable() {
+    var req = new XMLHttpRequest();
+    req.open("GET", "/all", true);
+
+    req.addEventListener("load", function(){
+        var response = JSON.parse(req.responseText);
+        var response = JSON.parse(response.results);
+        var updatedTable = buildTable(response);
+        body.removeChild(document.getElementById("mainTable"));
+        updatedTable.id = "mainTable";
+        body.appendChild(updatedTable);
+    });
+
+    req.send(null);
+    event.preventDefault();
+}
